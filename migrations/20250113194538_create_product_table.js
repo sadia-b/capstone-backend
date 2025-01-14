4783aa3f75cbfe0d0fbd628ead1b35c253320aff
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-01-13 21:33:14 -0500

    added data to the products table

--- migrations/20250113194538_create_product_table.js
+++ migrations/20250113194538_create_product_table.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("type").notNullable();
+    table.string("time").notNullable();
+    table.string("concern").notNullable();
+    table.string("brand").notNullable();
+    table.string("name").notNullable();
+    table.string("ingredient").notNullable();
+    table.double("price", 15, 2).notNullable();
+    table.string("photo").notNullable();
+    table.string("photoDescription").notNullable();
+    table.text("link").notNullable();
+  });
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("products");
+}
