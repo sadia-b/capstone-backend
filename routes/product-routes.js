7161a42b14e4437a5a76c0485245db22d7870043
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-01-17 01:40:25 -0500

    added endpoint to get products by concern

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -4,12 +4,25 @@ const knex = initKnex(configuration);
 import express from "express";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.route("/").get(async (req, res) => {
   try {
     const data = await knex("product");
     res.status(200).json(data);
   } catch (error) {
     res.status(400).send(`Error retrieving products: ${error}`);
+  }
+});
+
+router.route("/:concern").get(async (req, res) => {
+  try {
+    const products = await knex("product").where({
+      concern: req.params.concern,
+    });
+    res.json(products);
+  } catch (error) {
+    res.status(400).json({
+      message: `Error getting products for product ${req.params.concern}`,
+    });
   }
 });
 
