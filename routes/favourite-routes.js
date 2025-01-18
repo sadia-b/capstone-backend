512906302cdbbf9699d424262afa055d4b0d7f43
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2025-01-17 19:57:06 -0500

    added get favourites endpoint

--- routes/favourite-routes.js
+++ routes/favourite-routes.js
@@ -4,49 +4,75 @@ const knex = initKnex(configuration);
 import express from "express";
 const router = express.Router();
 
-router.route("/").post(async (req, res) => {
-  console.log(req.body);
+router
+  .route("/")
+  .post(async (req, res) => {
+    console.log(req.body);
 
-  const { product_id, user_id } = req.body;
+    const { product_id, user_id } = req.body;
 
-  if (!product_id || !user_id) {
-    return res
-      .status(400)
-      .json({ message: "Missing 'product_id' or 'user_id' field." });
-  }
+    if (!product_id || !user_id) {
+      return res
+        .status(400)
+        .json({ message: "Missing 'product_id' or 'user_id' field." });
+    }
 
-  try {
-    const [newFavouriteId] = await knex("favourite").insert({
-      product_id,
-      user_id,
-    });
+    try {
+      const [newFavouriteId] = await knex("favourite").insert({
+        product_id,
+        user_id,
+      });
 
-    console.log(newFavouriteId);
+      console.log(newFavouriteId);
 
-    const newFavouriteData = await knex("favourite")
-      .join("product", "product.id", "favourite.product_id")
-      .select(
-        "product.id",
-        "product.type",
-        "product.time",
-        "product.concern",
-        "product.brand",
-        "product.name",
-        "product.ingredient",
-        "product.price",
-        "product.photo",
-        "product.photoDescription",
-        "product.link"
-      )
-      .where("favourite.id", newFavouriteId);
+      const newFavouriteData = await knex("favourite")
+        .join("product", "product.id", "favourite.product_id")
+        .select(
+          "product.id",
+          "product.type",
+          "product.time",
+          "product.concern",
+          "product.brand",
+          "product.name",
+          "product.ingredient",
+          "product.price",
+          "product.photo",
+          "product.photoDescription",
+          "product.link"
+        )
+        .where("favourite.id", newFavouriteId);
 
-    res.status(201).json(newFavouriteData[0]);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ message: `Error favouriting product`, error: error.message });
-  }
-});
+      res.status(201).json(newFavouriteData[0]);
+    } catch (error) {
+      res
+        .status(400)
+        .json({ message: `Error favouriting product`, error: error.message });
+    }
+  })
+  .get(async (req, res) => {
+    try {
+      const favouritesData = await knex("favourite")
+        .join("product", "product.id", "favourite.product_id")
+        .select(
+          "product.id",
+          "product.type",
+          "product.time",
+          "product.concern",
+          "product.brand",
+          "product.name",
+          "product.ingredient",
+          "product.price",
+          "product.photo",
+          "product.photoDescription",
+          "product.link"
+        );
+      res.status(200).json(favouritesData);
+    } catch (error) {
+      res.status(400).json({
+        message: "Something went wrong while retrieving the favourites",
+      });
+    }
+  });
 
 router.route("/:id").delete(async (req, res) => {
   const itemId = req.params.id;
